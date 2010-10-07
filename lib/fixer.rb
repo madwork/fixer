01a2df4da457c01039411e0daa59706b29408640
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-10-07 09:43:19 +0100

    Adding a simple way to query for current fx rates

--- lib/fixer.rb
+++ lib/fixer.rb
@@ -4,8 +4,20 @@ require 'nokogiri'
 
 module Fixer
   class << self
+    def base_currency
+      @base_currency || "USD"
+    end
+
+    def base_currency=(name)
+      @base_currency = name
+    end
+
     def daily
       get('daily')
+    end
+
+    def expire_cache
+      @daily = nil
     end
 
     def historical
@@ -15,8 +27,21 @@ module Fixer
     def historical_90
       get('hist-90d')
     end
-      
+
+    def self.method_missing(sym, *args)
+      # the first argument is a Symbol, so you need to_s it if you want to pattern match
+      if method_sym.to_s =~ /^find_by_(.*)$/
+        find($1.to_sym => arguments.first)
+      else
+        super
+      end
+    end
+
     private
+
+    def cached_daily
+      @daily ||= daily
+    end
 
     def get(type)
       path  = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-#{type}.xml"
