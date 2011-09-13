08dcf6de1234aa54cd65b18f71086df09784f0cd
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-09-13 18:38:36 +0100

    a helper method to create indexes

--- lib/fixer/connection.rb
+++ lib/fixer/connection.rb
@@ -1,5 +1,11 @@
 module Fixer
   module Connection
+    def self.create_indexes
+      database.collections.select {|c| c.name !~ /system/ }.each do |c|
+        c.create_index([['date', Mongo::DESCENDING]])
+      end
+    end
+
     def self.database
       @database ||= Mongo::Connection.new['fixer']
     end
